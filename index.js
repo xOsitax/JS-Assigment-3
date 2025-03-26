4d46c8ec9fe563e3b980a2dea575dc73aad5a6db
YesseniaC <98070263+xOsitax@users.noreply.github.com>
2025-03-26 10:02:25 +0100

    password

---
+++ index.js
@@ -0,0 +1,19 @@
+function isValidPass(password, username) {
+  //check if password is 8 charactes
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Check if password contains spaces
+  if (password.includes(" ")) {
+    return false;
+  }
+
+  // Password cannot be the username
+  if (password.includes(username)) {
+    return false;
+  }
+
+  // If everything is fine, return true
+  return true;
+}
